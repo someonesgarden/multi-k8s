8d6d4ba32353e8c2a1c6820aad17a5d3f83ecc7b
Daisuke Nishimura <d@someonesgarden.org>
2019-05-10 22:00:14 +0900

    ingress-service.yaml fixed

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ const redisPublisher = redisClient.duplicate();
 // Express route handlers
 
 app.get('/', (req, res) => {
-  res.send('Hi');
+  res.send(0);
 });
 
 app.get('/values/all', async (req, res) => {
